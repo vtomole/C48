77efccb92dc3bed5d78aac6620895e750d1fc154
vtomole <vtomole@iastate.edu>
2017-10-30 20:33:22 -0500

    added updated utilsh and utils.c

---
+++ src/utils.h
@@ -0,0 +1,9 @@
+char* chopN(char *charBuffer, int n);
+char *scat(char *s,char *t);
+int iswhitespace (char c);
+char* append(char* str, char c);
+int count_chars(char* string, char ch);
+int our_isnumber (char *s);
+char *trimwhitespace(char *str);
+int isequal(char *s, char *t);
+int isnumber(char *s);
