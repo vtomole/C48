05b6d93c28679ef461483b326fd6a3d6f86cefc7
Zach Johnson <zed.johnson35@gmail.com>
2017-11-06 17:02:43 -0600

    Revert "changed how parse works is currently broken"

--- src/parser.c
+++ src/parser.c
@@ -171,26 +171,6 @@ int count_objects(object* cursor){
 }
 
 
-object* create_object_for_parse(char* type, char* value){
-
-  printf("DA TYPE %s\n", type);
-   if(strcmp(type,"primitive")==0){
-     return  create_primitiveop(value);
-   } else if(strcmp(type,"variable")==0){
-     return create_primitiveop(value);
-   } else if(strcmp(type,"num")==0){
-     return create_number(value);
-   } else if(strcmp(type,"string")==0){
-     return create_string(value);
-   } else if(strcmp(type,"boolean")==0){
-     return create_boolean(value);
-   } else {
-     printf("invalid token\n");
-     exit(0);
-   }
- }
-
-
 /**
  *Recursivly adds tokens to the code_tree
  * Parameters:
@@ -205,38 +185,50 @@ object* create_object_for_parse(char* type, char* value){
  * each statement returns a list containing all the statements before a right or left paren
  * no seg_faults however the code isn't doing anything visible with the temp_list it creates and maintains.
 **/
+object* parse_rec(token_list* token_list, object* list_sofar){
+  struct object *expr3;
+  struct object *temp_list;
 
-object* parse(token_list* token_list, object* expr){
-  object*  expr2;
- 
-
-  //  while(token_list != NULL){
-    printf("DAE VALUE %s\n", token_list->val.value);
+   while(token_list != NULL){
     if(strcmp(token_list->val.type,"right_paren")==0){
-      //start new recursion
-      expr2 = parse(token_list->next, expr);      
-      expr = cons(expr2, expr);
-    }else if(strcmp(token_list->val.type,"left_paren")==0){
-      //exit recursion
-      // token_list = token_list->next;
-      printf("THIS SHOULD PRINT ONCE\n");
-      return expr;
+      //indicates the start of a new list
+      expr3 = parse_rec(token_list->next, temp_list);
+      temp_list = cons(expr3, temp_list);
+    }else if(strcmp(token_list->val.type,"left_paren") == 0){
+      //end of a list
+      return temp_list;
+    }else if(strcmp(token_list->val.type,"primitive")==0){
+      //constructing an operator onto the list
+      expr3 = create_primitiveop(token_list->val.value);
+      temp_list = cons(expr3, temp_list); 
+    }else if(strcmp(token_list->val.type,"num")==0){
+      //constructing a number onto the list
+      expr3 = create_number(token_list->val.value);
+      temp_list = cons(expr3, temp_list);
+    }else if(strcmp(token_list->val.type,"string")==0){
+      //constructing a string onto the list
+      expr3 = create_string(token_list->val.value);
+      temp_list = cons(expr3, temp_list);     
+    }else if(strcmp(token_list->val.type,"boolean")==0){
+      //constructing a boolean onto the list
+      expr3 = create_boolean(token_list->val.value);
+      temp_list = cons(expr3, temp_list);      
     }else{
-      //add expression or prim to list
-      expr2 = create_object_for_parse(token_list->val.type,token_list->val.value);
-      expr = cons(expr2, expr);
-      printf("SHOULD BE + %d\n", car(expr)->number);
-      //expr = parse(token_list->next, expr);
-      //token_list = token_list->next;
+      printf("invalid token\n");
+      exit(0);
     }
-    // }
-  return expr;
+    token_list = token_list->next;
+  }
+   //throw an error no closed parenthesis
+   printf("invalid syntax\n");
+   exit(0);
+
 }
 
-  /*
-object* parse_tmp(token_list* token_list, object* expr_list){
+object* parse(token_list* token_list, object* expr_list){
   struct object *expr2;
-  print_token_list(token_list);
+
+  ///print_token_list_value(token_list);
 
    while(token_list != NULL){
     if(strcmp(token_list->val.type,"right_paren")==0){
@@ -286,6 +278,4 @@ object* parse_tmp(token_list* token_list, object* expr_list){
     token_list = token_list->next;
    }
    return expr_list;
-   }*/
-
-
+}
