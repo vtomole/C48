4462cc69359cdc2379ec08cd05635ce40d3c9f16
vtomole <vtomole@iastate.edu>
2017-11-22 21:15:40 -0600

    send repl.c

--- src/repl.c
+++ src/repl.c
@@ -6,74 +6,36 @@
 #include <assert.h>
 #include "lexer.c"
 #include "lexer_tmp.c"
-#include "parser.c"
+#include "environment.c"
 #include "comp_front.c"
-
-//#include "vm.h"
-//#include "vm.c"
-//#include "read_2.c"
-#include "eval-apply.c"
+#include "read.c"
+#include "eval.c"
 #include "print.c"
-#include "compiler.c"
+
 
 
 int main(int argc, char **argv){
-
-  char str[10000];
-  struct pair *list = NULL;
-  struct  token_list *token_list = NULL;
-  struct  token_list *token_list2 = NULL;
-  struct token_object object1;
-  struct object *expr_list;
-  struct object *expr;
-  struct object *expr2;
-  struct object *a;
-  struct object *h;
-  struct object *number;
-  struct object *list2;
-  struct object *result_expr = NULL;
-  object* code_tree = NULL;
-  object* environment = NULL;
-  object* environment2 = NULL;
-  object *cons1 = NULL;
-  object *temp_list = NULL;
-  char* operator = "third";
-  char* string = "\"A string\"";
-  int b = 20, c = 30;
-  // hashtable_t *env = ht_create( 65536 );
-  char* result = NULL;
-
-  object *add1 = create_primitiveop("+");
-  object *num11 = create_number("4");
-  object *num22 = create_number("5");
-
+  char str[1000];
+  object* cons1;
+  token_list* token_list;
+  char* res;
+  FILE  *fp, *fp1, *fp2;
   
+  initialize_environment();
+  fp1 = fopen("hello.c48", "r");
+  fgets (str, 10000, fp1);
+  res = prefix(str);
+  fclose(fp1);
+  fp2 = fopen("hello.sexpr", "w");
+  fprintf(fp2, "%s", res);
+  fclose(fp2);
+  fp = fopen("hello.sexpr", "r");
   
-  temp_list = cons(num22, temp_list);
-  temp_list = cons(num11, temp_list);
-  temp_list = cons(add1, temp_list);
-  
-  cons1 = parse(lexer_tmp("( + 4 5 )"),result_expr);
-  printf("Should be + %d\n", car(cons1)->number);
-  //printf("Should be cons %d\n", car(cdr(cons1))->number);
-
-  
-
- 
-  /*while (1){     
-    printf("repl>");
-    fgets (str, 10000, stdin);
-    printf("=>");
-    //micro_read(str);
-    //print(eval(parse(lexer_tmp(prefix(str)), expr_list),environment));
-    object* cons1 = parse(lexer_tmp(str), expr_list);
-     printf("Should be + %s\n", car(cons1)->variable);
-     printf("Should be cons %s\n", car(cdr(cons1))->type);
-     printf("Should be + %s\n", car(car(cdr(cons1)))->variable);
-     printf("Should be 4 %d\n", car(cdr(car(cdr(cons1))))->number);
-    print(eval(parse(lexer_tmp(str), expr_list),environment));
-    
-    }*/
+  while (1) {
+    printf("repl> ");
+    print(eval(read(stdin), environment));
+    printf("\n");
+  }    
   return 0;
 }//end of main>
 
