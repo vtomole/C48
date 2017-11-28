a0719a94a1ff33acf27a07e623ca9b03f701e991
Hzey <hzey@iastate.edu>
2017-11-28 16:27:28 -0600

    added a list to keep track of elements which need fast forwarding in token_list to code_tree

--- src/parser.c
+++ src/parser.c
@@ -206,23 +206,34 @@ object* create_object_for_parse(char* type, char* value){
  * no seg_faults however the code isn't doing anything visible with the temp_list it creates and maintains.
 **/
 
+
+// Creates an node to count the number of inputs evaluated at the recursion
+// level of the token_list evaluation. This helps fix the issue of the pointer
+// to the token_list not fast fowarding when a branch is added to our code_tree.
 typedef struct fast_forward_node{
   int count;
   struct fast_forward_node* next;
 }
 
+// This creates a node with a count of 0 to indicate that it coutains 0 elements in its chain,
+// and thus fast fowarding is not needed **yet**. The next node will point to the next element to
+// be evaluated. This will almost always be passed NULL.
 fast_forward_node* create_node(fast_forward_node* next){
   fast_forward_node* new_node = (fast_forward_node*)malloc(sizeof(fast_foward_node));
-  new_node->count = 0;
+  new_node->count = 1;
   new_node->next = next;
 }
 
+// This adds a node to the front of the list. This is used to initialize the list
+// and put head at the front.
 fast_forward_node* prepend_node(fast_forward_node* head){
   fast_forward_node* new_node = create_node(head);
   head = new_node;
   return head;
 }
 
+// This gets the last element in the list. 
+// ex: head -> node -> node -> this
 fast_forward_node* get_inner_node(fast_forward_node* head){
   fast_forward_node* cursor = head;
   while(cursor->next != NULL)
@@ -230,13 +241,21 @@ fast_forward_node* get_inner_node(fast_forward_node* head){
   return cursor;
 }
 
+// This gets the 2nd to last element in the list. 
+// ex: head -> node -> this -> node
+fast_forward_node* get_2nd_to_last_node(fast_forward_node* head){
+  fast_forward_node* cursor = head;
+  while(cursor->next->next != NULL)
+    cursor = cursor->next;    
+  return cursor;
+}
+
+// This adds a new node to the end of the list.
 fast_forward_node* apppend_node(fast_forward_node* head, fast_foward_node* new_node){
   fast_forward_node* cursor = get_inner_node(head);
   cursor->next = new_node;
   return head;
 }
-
-void update_count(
 
 //global variables
 fast_forward_node* head;
@@ -254,26 +273,46 @@ object* parse(token_list* token_list, object* expr){
 
   if(strcmp(token_list->val.type,"right_paren")==0){
     token_list = token_list->next;
-    /*if(head->next == NULL)
-      head->count++;
-    else{
-      fast_forward_node* new_node = create_node(NULL);
-      new_node->count++;
-      append_node(head);
-      }*/
-    expr2 = parse(token_list, expr);//recurse 
+    
+    fast_forward_node* new_node = create_node(NULL); //create a node for recursive tracking
+    append_node(get_inner_node(head)); //add this node to the end of the list
+    get_inner_node(head)->count++;//add 1 to end node
+
+    expr2 = parse(token_list, expr);//the recursion of all nested elements
+
+    for(int i = 0; i < get_inner_node(head)->count; i++) //fast forward for each element in the above recusion
+      token_list = token_list->next;//fast forward one element
+
+    // Add the last nodes element count to the 2nd to last nodes elements count. This
+    // is in order to account for the 2nd to last node needing these elements. Each
+    // node represents a layer of recursion, and each layer needs to be fast forwarded separately.
+    // Thus adding this element count allows outer layers to be fast forwarded to account
+    // for possible inner recursive layers.
+    if(head->next->next != NULL){
+      get_2nd_to_last_node(head)->count += get_inner_node(head)->count; 
+      get_2nd_to_last_node(head)->next = NULL; //remove recursive layer(represented as the node)
+    }else if(head->next != NULL){
+      head->count += get_inner_node(head)->count; 
+      head->next = NULL; //remove recursive layer(represented as the node)
+    }
+    free(get_inner_node(head)->next);//remove last node from memory
+    
     printf("hi");
     expr = cons(expr2, expr);//attach
 
   }else if(strcmp(token_list->val.type,"left_paren")==0){
+
+    get_inner_node(head)->count++;//add 1 to end node
     printf("\n");
     return expr;//exit recurse
 
   }else if(strcmp(token_list->val.type,"primitive")==0){
+    get_inner_node(head)->count++;//add 1 to end node
     expr2 = create_object_for_parse(token_list->val.type,token_list->val.value);
     expr = cons(car(expr2), cons(expr2, expr));
 
   }else{
+    get_inner_node(head)->count++;//add 1 to end node
     //add expression or prim to list
     expr2 = create_object_for_parse(token_list->val.type,token_list->val.value);
     expr = cons(expr2,expr);//attach previous to this car
