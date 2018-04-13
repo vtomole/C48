108a3f24f63ac1621c16f7ffe99cbbf37de9056c
vtomole <vtomole@iastate.edu>
2018-04-13 13:01:26 -0500

    typechecker works on compound functions. Need to fix errors for predicate procedures in environment

--- src/typechecker.c
+++ src/typechecker.c
@@ -8,13 +8,29 @@ object *eval_assignmenttp(object *exp, object *env) {
     return exp;
 }
 
+object *goto_end (object *exp) {
+  object *prev;
+  object *next;
+
+  next = exp;
+
+  while (!is_the_empty_list(next)) {
+    prev = car(next);
+    next = cdr(next);
+  }
+  //print(prev);
+
+  return exp;
+
+}
+
 object *eval_definitiontp(object *exp, object *env) {
     define_variable(definition_variable(exp), 
                     eval(definition_value(exp), env),
                     env);
-    //print(exp);
     return exp;
 }
+
 
 object *typecheck(object *exp, object *env) {
     object *procedure;
@@ -23,43 +39,44 @@ object *typecheck(object *exp, object *env) {
     object *primary_type;
     object *acceptables;
     object *type_arguments;
+    object* static_exp;
+    object *end_list;
+
+    static_exp = exp;
 
 tailcall:
     if (self_evaluatingp(exp)) {
-        return exp;
+        return static_exp;
     }
     else if (variablep(exp)) {
-      type_object = lookup_variable_value(exp, env);
-      printf("Type of object %d\n", type_object->obj_type);
-      return type_object;
+      return lookup_variable_value(exp, env);
     }
     else if (is_quoted(exp)) {
-      printf("It's quoted \n");
-        return text_of_quotation(exp);
+        return static_exp;
     }
     else if (is_assignment(exp)) {
-      printf("It's an assignment \n");
-        return eval_assignmenttp(exp, env);
+      //printf("It's an assignment \n");
+      return eval_assignmenttp(exp, env);
     }
     else if (is_definition(exp)) {
       //printf("It's define \n");
         return eval_definitiontp(exp, env);
     }
     else if (is_if(exp)) {
-      printf("It's if \n");
+      //printf("It's if \n");
         exp = is_true(eval(if_predicate(exp), env)) ?
                   if_consequent(exp) :
                   if_alternative(exp);
         goto tailcall;
     }
     else if (is_lambda(exp)) {
-      printf("It's lambda \n");
+      //printf("It's lambda \n");
         return make_compound_proc(lambda_parameters(exp),
                                   lambda_body(exp),
                                   env);
     }
     else if (is_begin(exp)) {
-      printf("It's begin \n");
+      //printf("It's begin \n");
         exp = begin_actions(exp);
         while (!is_last_exp(exp)) {
             eval(first_exp(exp), env);
@@ -69,12 +86,12 @@ tailcall:
         goto tailcall;
     }
     else if (is_cond(exp)) {
-      printf("It's cond \n");
+      //printf("It's cond \n");
         exp = cond_to_if(exp);
         goto tailcall;
     }
     else if (is_let(exp)) {
-      printf("It's let \n");
+      //printf("It's let \n");
         exp = let_to_application(exp);
         goto tailcall;
     }
@@ -84,7 +101,6 @@ tailcall:
         arguments = list_of_values(operands(exp), env);
 	type_arguments = list_of_values(operands(exp), env);
         if (primitivep(procedure)) {
-	  //printf("It's primitive\n");
 	  acceptables = procedure->primitive_proc.acceptables;
 	  
 	  primary_type = car(cdr(type_arguments));
@@ -96,15 +112,14 @@ tailcall:
 	    }	      
 	    type_arguments = cdr(type_arguments);
 	  }
-	  //printf("%d\n",car(acceptables)->number);
+	  
 	  type_arguments = list_of_values(operands(exp), env);
 
 	  int type_flag = 0;
 
 	  while (!is_the_empty_list(acceptables)) {
-	   // printf("%d\n",car(type_arguments->primitive_proc.acceptables)->number);
+	    
 	     if(primary_type->obj_type == car(acceptables)->number){
-	      	     //fprintf(stderr, "Type error\n");
 	       type_flag = 1;
 	      }	     
 	    acceptables = cdr(acceptables);
@@ -114,16 +129,20 @@ tailcall:
 	    fprintf(stderr, "Type error variables\n");
 	    exit(1);
 	  }
-	  
-	  
-          return exp;
+
+	  //printf("Static exp\n");
+	  //print(static_exp);
+          return static_exp;
+       
         }
         else if (is_compound_proc(procedure)) {
+	  
             env = extend_environment( 
                        procedure->compound_proc.parameters,
                        arguments,
                        procedure->compound_proc.env);
             exp = make_begin(procedure->compound_proc.body);
+	    
             goto tailcall;
         }
         else {
