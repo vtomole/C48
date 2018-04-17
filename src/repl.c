a620a424e6ee13c78a5b2c0d90b39a94fce9969e
vtomole <vtomole@iastate.edu>
2018-04-17 18:22:12 -0500

    numbers and repl

--- src/repl.c
+++ src/repl.c
@@ -32,29 +32,29 @@ int main(int argc, char **argv){
     print(eval(typecheck(read(stdin), the_global_environment), the_global_environment));
     
     }*/
-  object *temp_list = the_empty_list;
+  /*object *temp_list = the_empty_list;
   object *add =  make_symbol(">");
   object *num1 = make_string("apple");
   object *num2 = make_string("banana");
   temp_list = cons(num2, temp_list);
   temp_list = cons(num1, temp_list);
   temp_list = cons(add, temp_list);
-  print(eval(typecheck(temp_list, the_global_environment),the_global_environment));
+  print(eval(typecheck(temp_list, the_global_environment),the_global_environment));*/
   //print(temp_list);
 
-  //fp = fopen("array.scm", "r");
-  //object *exp;
+  fp = fopen(argv[1], "r");
+  object *exp;
 
   
 
-  /*while ((exp = read(fp)) != NULL) {
+  while ((exp = read(fp)) != NULL) {
     //print(exp);
     //print(eval(typecheck(exp, the_global_environment), the_global_environment));
     //print(typecheck(exp, the_global_environment));
     print(eval(exp, the_global_environment));
     }
 
-  fclose(fp);*/
+  fclose(fp);
 
   //object *obj = cons(make_symbol("+"), cons(make_fixnum(4), cons(make_fixnum(5), the_empty_list)));
   //print(obj);
