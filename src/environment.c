ea8400a84ecc68358f8dd8f7bb89e2c17396cddf
hzey <hzey@iastate.edu>
2018-04-22 16:44:14 -0500

    moved defines and global vars to .h

--- src/environment.c
+++ src/environment.c
@@ -53,29 +53,6 @@ object* alloc_object(){
 
 }
 
-/*global variables*/
-object *the_empty_list;
-object *false;
-object *true;
-object *symbol_table;
-object *quote_symbol;
-object *define_symbol;
-object *set_symbol;
-object *ok_symbol;
-object *if_symbol;
-object *lambda_symbol;
-object *begin_symbol;
-object *cond_symbol;
-object *else_symbol;
-object *let_symbol;
-object *the_empty_environment;
-object *the_global_environment;
-
-/*function delcarations*/
-object *cons(object *car, object *cdr);
-object *car(object *pair);
-object *cdr(object *pair);
-
 //Boolean Functions
 char is_the_empty_list(object *obj) { return obj == the_empty_list; }
 char booleanp(object *obj) { return obj->obj_type == BOOLEAN; }
@@ -228,35 +205,6 @@ object *car(object *pair) { return pair->cons_cell.car; }
 object *cdr(object *pair) { return pair->cons_cell.cdr; }
 void set_car(object *obj, object* value) { obj->cons_cell.car = value; }
 void set_cdr(object *obj, object* value) {obj->cons_cell.cdr = value; }
-
-#define caar(obj)   car(car(obj))
-#define cadr(obj)   car(cdr(obj))
-#define cdar(obj)   cdr(car(obj))
-#define cddr(obj)   cdr(cdr(obj))
-#define caaar(obj)  car(car(car(obj)))
-#define caadr(obj)  car(car(cdr(obj)))
-#define cadar(obj)  car(cdr(car(obj)))
-#define caddr(obj)  car(cdr(cdr(obj)))
-#define cdaar(obj)  cdr(car(car(obj)))
-#define cdadr(obj)  cdr(car(cdr(obj)))
-#define cddar(obj)  cdr(cdr(car(obj)))
-#define cdddr(obj)  cdr(cdr(cdr(obj)))
-#define caaaar(obj) car(car(car(car(obj))))
-#define caaadr(obj) car(car(car(cdr(obj))))
-#define caadar(obj) car(car(cdr(car(obj))))
-#define caaddr(obj) car(car(cdr(cdr(obj))))
-#define cadaar(obj) car(cdr(car(car(obj))))
-#define cadadr(obj) car(cdr(car(cdr(obj))))
-#define caddar(obj) car(cdr(cdr(car(obj))))
-#define cadddr(obj) car(cdr(cdr(cdr(obj))))
-#define cdaaar(obj) cdr(car(car(car(obj))))
-#define cdaadr(obj) cdr(car(car(cdr(obj))))
-#define cdadar(obj) cdr(car(cdr(car(obj))))
-#define cdaddr(obj) cdr(car(cdr(cdr(obj))))
-#define cddaar(obj) cdr(cdr(car(car(obj))))
-#define cddadr(obj) cdr(cdr(car(cdr(obj))))
-#define cdddar(obj) cdr(cdr(cdr(car(obj))))
-#define cddddr(obj) cdr(cdr(cdr(cdr(obj))))
 
 /**char_to_enum()
  * this function is used determine the enumuration of the sent in char pointer
--- src/environment.h
+++ src/environment.h
@@ -1,3 +1,32 @@
+#define caar(obj)   car(car(obj))
+#define cadr(obj)   car(cdr(obj))
+#define cdar(obj)   cdr(car(obj))
+#define cddr(obj)   cdr(cdr(obj))
+#define caaar(obj)  car(car(car(obj)))
+#define caadr(obj)  car(car(cdr(obj)))
+#define cadar(obj)  car(cdr(car(obj)))
+#define caddr(obj)  car(cdr(cdr(obj)))
+#define cdaar(obj)  cdr(car(car(obj)))
+#define cdadr(obj)  cdr(car(cdr(obj)))
+#define cddar(obj)  cdr(cdr(car(obj)))
+#define cdddr(obj)  cdr(cdr(cdr(obj)))
+#define caaaar(obj) car(car(car(car(obj))))
+#define caaadr(obj) car(car(car(cdr(obj))))
+#define caadar(obj) car(car(cdr(car(obj))))
+#define caaddr(obj) car(car(cdr(cdr(obj))))
+#define cadaar(obj) car(cdr(car(car(obj))))
+#define cadadr(obj) car(cdr(car(cdr(obj))))
+#define caddar(obj) car(cdr(cdr(car(obj))))
+#define cadddr(obj) car(cdr(cdr(cdr(obj))))
+#define cdaaar(obj) cdr(car(car(car(obj))))
+#define cdaadr(obj) cdr(car(car(cdr(obj))))
+#define cdadar(obj) cdr(car(cdr(car(obj))))
+#define cdaddr(obj) cdr(car(cdr(cdr(obj))))
+#define cddaar(obj) cdr(cdr(car(car(obj))))
+#define cddadr(obj) cdr(cdr(car(cdr(obj))))
+#define cdddar(obj) cdr(cdr(cdr(car(obj))))
+#define cddddr(obj) cdr(cdr(cdr(cdr(obj))))
+
 typedef enum {EMPTY_LIST, BOOLEAN, SYMBOL, FIXNUM, FLOAT,
               CHARACTER, STRING, PAIR, PRIMITIVE_PROC,
               COMPOUND_PROC} object_type;
@@ -33,7 +62,28 @@ typedef struct object{
   
 }object;
 
+/*global variables*/
+object *the_empty_list;
+object *false;
+object *true;
+object *symbol_table;
+object *quote_symbol;
+object *define_symbol;
+object *set_symbol;
+object *ok_symbol;
+object *if_symbol;
+object *lambda_symbol;
+object *begin_symbol;
+object *cond_symbol;
+object *else_symbol;
+object *let_symbol;
+object *the_empty_environment;
+object *the_global_environment;
 
+/*function delcarations*/
+object *cons(object *car, object *cdr);
+object *car(object *pair);
+object *cdr(object *pair);
 
 object* cons(object *car, object *cdr);
 void  initialize_environment(void);
