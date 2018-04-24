bc0c5e21f3455ebfc8b554796e43e6815e82afde
ztwild <ztwild@iastate.edu>
2018-04-24 04:36:23 +0000

    parser makes objects directly

--- src/convert.c
+++ src/convert.c
@@ -1,191 +1,61 @@
-#include "convert.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
 
-object *convert_symlist(struct symlist *syms){
-  if(syms == NULL){ return the_empty_list; }
-  object *obj = make_symbol(syms->sym->name);
-  return cons(obj, convert_symlist(syms->next));
+#include "environment.c"
+#include "eval-apply.c"
+#include "print.c"
+
+
+
+
+object *get_array_index(char *name, object *index){
+  object *sym = make_symbol("nth");
+  object *n = make_symbol(name);
+  return cons(sym, cons(index, cons(n, the_empty_list)));
+}
+
+object *set_array_index(char *name, object *index, object *expr){
+  object *i = get_array_index(name, index);
+  return cons(define_symbol, cons(i, cons(expr, the_empty_list)));
+}
+
+object *make_define_array(char *name, object *list){
+  object *n = make_symbol(name);
+  list = cons(quote_symbol, cons(list, the_empty_list));
+  return cons(define_symbol, cons(n , list));
+}
+
+object *make_assign(char *name, object *expr){
+  object *n = make_symbol(name);
+  return cons(define_symbol, cons(n, cons(expr, the_empty_list)));
+}
+
+object *make_arith(char *op, object *first, object *second){
+  object *sym = make_symbol(op);
+  return cons(sym, cons(first, cons(second, the_empty_list)));
 }
 
 
-object *convert_func(struct symbol *func){
-  object *b, *fun, *params, *expr;
-
-  params = convert_symlist(func->syms);
-  expr = convert_expr(func->func);
-  
-  fun = cons(make_symbol(func->name), params);
-  
-  return cons(define_symbol, 
-              cons(fun, 
-                    expr 
-                        ));         
+object *make_comp(char *cmp, object *first, object *second){
+  object *sym = make_symbol(cmp);
+  return cons(sym, cons(first, cons(second, the_empty_list)));
 }
 
-object *convert_exprlist(struct ast *a){
-  if(!a){
-    return the_empty_list;
-  }
-  else if(a->nodetype == 'L') {
-    object *obj = convert_expr(a->l);
-    return cons(obj, convert_exprlist(a->r));
-  }
-  else {
-    object *obj = convert_expr(a);
-    return cons(obj, the_empty_list);
-  }
+object *make_while_loop(object *cond, object *expr){
+  object *w = make_symbol("while");
+  return cons(w, cons(cond, cons(expr, the_empty_list)));
 }
 
-object *convert_funccall(struct ufncall *f){
-  object *name = make_symbol(f->s->name);
-  object *list = convert_exprlist(f->l);
-  return cons(name, list);
-}
-
-object *convert_builtinfunc(struct fncall *f){
-  char *str;
-  switch(f->functype){
-  case B_sqrt:
-     str = "sqrt";
-     break;
-   case B_exp:
-     str = "exp";
-     break;
-   case B_log:
-     str = "log";
-     break;
-   case B_print:
-     str = "print";
-     break;
-  }
-  
-  object *name = make_symbol(str);
-  object *list = convert_exprlist(f->l);
-  return cons(name, list);
-}
-
-object *convert_varexpr(struct ast* a){
- object *var = make_symbol(((struct symasgn*)a)->s->name);
- object *expr = convert_expr(((struct symasgn*)a)->v);
- //object *varexpr = cons(var, cons(expr, the_empty_list));
- //varexpr = cons(varexpr, the_empty_list);
- return cons(define_symbol, cons(var, cons(expr, the_empty_list))); 
+object *make_define_func(char *name, object *params, object *expr){
+  object *n = make_symbol(name);
+  object *fun = cons(n, params);
+  return cons(define_symbol, cons(fun, expr));
 }
 
 
-
-object *convert_expr(struct ast* a){
-  object *car, *cdr, *op, *obj, *sym, *name, *list;
-  object *cond, *tl, *el;
-  char *operation = malloc(sizeof(char));
-  char *val;
-  int num;
-  //printf("node type %c\n", a->nodetype);
-  if(!a)
-    return the_empty_list;
-  
-  switch(a->nodetype){
-  case 'K': 
-    num = (float)((struct numval *)a)->number;
-    return make_float(num);
-  case 'M':
-    num = (float)-((struct numval *)a)->number;
-    return make_float(num);
-  case 'N':
-    val = ((struct symref *)a)->s->name;
-    return make_symbol(val);
-  
-  case 'A':
-    car = make_symbol(((struct arraylist *)a)->name->name);
-    cdr = convert_exprlist(((struct arraylist *)a)->exp);
-    list = cons(quote_symbol, (cons(cdr, the_empty_list)));
-    obj = cons(define_symbol, cons(car, cons(list, the_empty_list)));
-    return obj;
-  case 'S':
-    sym = make_symbol("nth");
-    name = make_symbol(((struct arraylist *)a)->name->name);
-    car = convert_expr(((struct arraylist *)a)->index);
-    car = cons(sym, cons(car, cons(name, the_empty_list)));
-    cdr = convert_expr(((struct arraylist *)a)->exp);
-    return cons(define_symbol, cons(car, cons(cdr, the_empty_list)));
-  case 'G':
-    sym = make_symbol("nth");
-    name = make_symbol(((struct arraylist *)a)->name->name);
-    car = convert_expr(((struct arraylist *)a)->index);
-    return cons(sym, cons(car, cons(name, the_empty_list)));
-      
-  
-  case 'C':
-    return convert_funccall((struct ufncall*)a);
-  case 'F':
-    return convert_builtinfunc((struct fncall*)a);
-  case '=':
-    //return cons(convert_varexpr(a), the_empty_list);
-    return convert_varexpr(a);
-  case 'L':
-    //car = cons(convert_expr(a->l), the_empty_list);
-    car = convert_expr(a->l);
-    //cdr = cons(convert_expr(a->r), the_empty_list);
-    cdr = convert_expr(a->r);
-    /*if(a->r->nodetype != 'L')
-	cdr = cons(cdr, the_empty_list);
-    object *temp = car;
-    while(temp->cons_cell.cdr != the_empty_list){
-      temp = temp->cons_cell.cdr;
-    }
-    temp->cons_cell.cdr = cdr;*/
-    return cons(car, cons(cdr, the_empty_list));
-  /** Creates Operation Objects**/
-  case '+': case '-': case '*':  case '/':  case '%':
-    car = convert_expr(a->l);
-    cdr = convert_expr(a->r);
-    sprintf(operation, "%c", a->nodetype);
-    op = make_symbol(operation);
-    obj = cons(op, cons(car, cons(cdr, the_empty_list)));
-    return obj;
-
-  /** Creates Comparison Object **/
-  case '1': 
-    car = convert_expr(a->l);
-    cdr = convert_expr(a->r);
-    op = make_symbol(">");
-    return cons(op, cons(car, cons(cdr, the_empty_list)));
-  case '2': 
-    car = convert_expr(a->l);
-    cdr = convert_expr(a->r);
-    op = make_symbol("<");
-    return cons(op, cons(car, cons(cdr, the_empty_list)));
-  case '3': 
-    car = convert_expr(a->l);
-    cdr = convert_expr(a->r);
-    op = make_symbol("!=");
-    return cons(op, cons(car, cons(cdr, the_empty_list)));
-  case '4': 
-    car = convert_expr(a->l);
-    cdr = convert_expr(a->r);
-    op = make_symbol("=");
-    return cons(op, cons(car, cons(cdr, the_empty_list)));
-  case '5': 
-    car = convert_expr(a->l);
-    cdr = convert_expr(a->r);
-    op = make_symbol(">=");
-    return cons(op, cons(car, cons(cdr, the_empty_list)));
-  case '6':
-    car = convert_expr(a->l);
-    cdr = convert_expr(a->r);
-    op = make_symbol("<=");
-    return cons(op, cons(car, cons(cdr, the_empty_list)));
-  case 'W':
-    cond = convert_expr(((struct flow*)a)->cond);
-    car = make_symbol("while");
-    cdr = convert_expr(((struct flow*)a)->tl);
-    return cons(car, cons(cond, cons(cdr, the_empty_list)));
-  case 'I':
-    cond = convert_expr(((struct flow*)a)->cond);
-    tl = convert_expr(((struct flow*)a)->tl);
-    el = convert_expr(((struct flow*)a)->el);
-    return make_if(cond, tl, el);
-  }
-}
 
 
 
--- src/parser.y
+++ src/parser.y
@@ -1,82 +1,77 @@
 %{
 #  include <stdio.h>
 #  include <stdlib.h>
-#  include "expression.h"
+#  include "expression.h" 
 #  include "convert.c"
 %}
 
 %union {
-  struct ast *a;
+  struct object *obj;
   double d;
-  struct symbol *s;		/* which symbol */
-  struct symlist *sl;
-  int fn;			/* which function */
+  char *s;
 }
 
 /* declare tokens */
-%token <d> NUMBER
-%token <s> NAME
-%token <fn> FUNCT
+%token <d> NUMBER DOUBLE 
+%token <s> NAME ARITH_1 ARITH_2 CMP_1 CMP_2 FUNCT STR
 %token EOL 
 
-%token IF THEN ELSE WHILE DO FUN FOR END RETURN
+%token IF THEN ELSE WHILE DO FUN FOR RETURN
 
-
-%nonassoc <fn> CMP
+%nonassoc CMP_1
+%nonassoc CMP_2
 %right '='
-%left '+' '-'
-%left '*' '/' '%'
+%left ARITH_1
+%left ARITH_2
 %nonassoc '|' UMINUS
 
-%type <a> exp stmt list explist
-%type <sl> symlist
+%type <obj> exp stmt list symlist explist
 
 %start calclist
 
 %%
 
-stmt:IF '(' exp ')' '{' list '}'                   { $$ = newflow('I', $3, $6, NULL); }
-   | IF '(' exp ')'  '{' list '}' ELSE '{'list '}'  { $$ = newflow('I', $3, $6, $10); }
-   | IF '(' exp ')'  '{' list '}' ELSE stmt         { $$ = newflow('I', $3, $6, $9); }
-   | WHILE '(' exp ')'  '{' list '}'                { $$ = newflow('W', $3, $6, NULL); }    
+stmt: WHILE '(' exp ')'  '{' list '}'          { $$ = make_while_loop($3, $6); }    
    | exp ';'
+   //| IF '(' exp ')' '{' list '}'                   { $$ = newflow('I', $3, $6, NULL); }
+   //| IF '(' exp ')'  '{' list '}' ELSE '{'list '}'  { $$ = newflow('I', $3, $6, $10); }
+   //| IF '(' exp ')'  '{' list '}' ELSE stmt         { $$ = newflow('I', $3, $6, $9); }
 ;
 
-list: /* nothing */ { $$ = NULL; }
-   | stmt list { if ($2 == NULL) $$ = $1; else $$ = newast('L', $1, $2); }
+list: /* nothing */ { $$ = the_empty_list; }
+   | stmt list      { $$ = cons($1, $2); }
    ;
 
-exp: exp CMP exp          { $$ = newcmp($2, $1, $3); }
-   | exp '+' exp          { $$ = newast('+', $1,$3); }
-   | exp '-' exp          { $$ = newast('-', $1,$3);}
-   | exp '*' exp          { $$ = newast('*', $1,$3); }
-   | exp '/' exp          { $$ = newast('/', $1,$3); }
-   | exp '%' exp          { $$ = newast('%', $1,$3); }
-   | '|' exp              { $$ = newast('|', $2, NULL); }
+exp: exp CMP_1 exp          { $$ = make_comp($2, $1, $3); }
+   | exp CMP_2 exp          { $$ = make_comp($2, $1, $3); }
+   | exp ARITH_1 exp       { $$ = make_arith($2, $1, $3); }
+   | exp ARITH_2 exp       { $$ = make_arith($2, $1, $3); }
    | '(' exp ')'          { $$ = $2; }
-   | '-' exp %prec UMINUS { $$ = newast('M', $2, NULL); }
-   | NUMBER               { $$ = newnum($1); }
-   | FUNCT '(' explist ')'{ $$ = newfunc($1, $3); }
-   | NAME                 { $$ = newref($1); }
-   | NAME '=' exp         { $$ = newasgn($1, $3); }
-   | NAME '(' explist ')' { $$ = newcall($1, $3); }
+   | '-' exp %prec UMINUS { $$ = make_arith("-", make_fixnum(0), $2); }
+   | NUMBER               { $$ = make_fixnum($1); }
+   | DOUBLE               { $$ = make_float($1); }
+   | NAME                 { $$ = make_symbol($1); }
+   | NAME '=' exp         { $$ = make_assign($1, $3); }
+   | NAME '(' explist ')' { $$ = cons(make_symbol($1), $3); }
+   | FUNCT '(' explist ')'{ $$ = cons(make_symbol($1), $3); }
+   | '|' exp              { $$ = cons(make_symbol("|"), $2); }
    | RETURN exp           { $$ = $2; }
+   | STR                  { $$ = make_symbol($1); }
    | NAME '['']' '=' '[' explist ']'
-                          { $$ = newarraylist($1, $6); }
-   | NAME '[' exp ']'     { $$ = getarrayindex($1, $3); }
+                          { $$ = make_define_array($1, $6); }
+   | NAME '[' exp ']'     { $$ = get_array_index($1, $3); }
    | NAME '[' exp ']' '=' exp
-                          { $$ = setarrayindex($1, $3, $6); }
-   
+                          { $$ = set_array_index($1, $3, $6); }
 ;
 
-
-explist:            { $$ = NULL; }
- | exp
- | exp ',' explist  { $$ = newast('L', $1, $3); }
+explist:            { $$ = the_empty_list; }
+ | exp              { $$ = cons($1, the_empty_list); }
+ | exp ',' explist  { $$ = cons($1, $3); }
 ;
-symlist:      { $$ = NULL; }
- | NAME       { $$ = newsymlist($1, NULL); }
- | NAME ',' symlist { $$ = newsymlist($1, $3); }
+
+symlist:            { $$ = the_empty_list; }
+ | NAME             { $$ = make_symbol($1); }
+ | NAME ',' symlist { $$ = cons(make_symbol($1), $3); }
 ;
 
 calclist: /* nothing */
@@ -84,20 +79,20 @@ calclist: /* nothing */
   | calclist stmt
     //{ if(debug) dumpast($2, 0); printf("= %4.4g\n> ", eval_ast($2)); treefree($2); }
     {
-      if(debug) dumpast($2, 0);
-      print(convert_expr($2));
-      eval(convert_expr($2), the_global_environment);
-      treefree($2);\
+      print($2);
+      printf("\n");
+      //eval($2, the_global_environment);
     }
             
   | calclist FUN NAME '(' symlist ')' '{' list '}' 
     //{ dodef($3, $5, $8); printf("Defined %s\n> ", $3->name); }
 	  {
-	    dodef($3, $5, $8);
-	    print(convert_func($3));
-	    eval(convert_func($3), the_global_environment);
+	    struct object *function = make_define_func($3, $5, $8);
+	    print(function);
+	    printf("\n");
+	    eval(function, the_global_environment);
 	  }
   
-  | calclist error  { yyerrok; printf("> "); }
+  | calclist error EOL { yyerrok; }
  ;
 %%
