b4bf5861b7bf4a3fd566d837625373b5eea7b652
ztwild <ztwild@iastate.edu>
2018-04-25 02:06:23 +0000

    removed read_file.c and put it into expression.c

--- src/expression.c
+++ src/expression.c
@@ -4,7 +4,32 @@
 #include <string.h>
 #include <math.h>
 #include "expression.h"
-#include "read_file.c"
+
+
+/** open_file()
+ * Opens the code file for the lexer and parser
+ * to convert to s-expressions
+ * Parameters:
+ * -name, the name of the code file 
+ * Returns:
+ * int, 0 if the file does not exist, 1 if it opened successfully
+ */
+int open_file(const char *name){
+  
+  int length = strlen(name) + strlen("./") + 1;
+  char *cpy = malloc(sizeof(char) * length);
+  strcpy(cpy, "./");
+  strcat(cpy, name);
+  //printf("trying to open %s\n", cpy);
+  FILE *file;
+  file = fopen(cpy, "r");
+  if(!file){
+    fprintf(stderr, "File does not exist.");
+    return 0;
+  }
+  yyset_in(file);
+  return 1;
+}
 
 /** yyerror()
  * Prints error message and 
